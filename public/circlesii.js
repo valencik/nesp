8b32571e4c0ffdff377ccffbf2a0d2c6bb9874d6
Michaelangelo Jong <mike96jong@gmail.com>
2014-02-23 03:11:36 -0500

    Added client side controller support

--- public/circlesii.js
+++ public/circlesii.js
@@ -66,3 +66,175 @@ function stop() {
     }
 })();
 
+function down(key){
+    console.log(key, 1);
+}
+function up(key){
+    console.log(key, 0);
+}
+
+
+window.onkeydown = function (evt) {
+    toggleKey(evt.keyCode, 1);
+    console.log(evt.keyCode);
+};
+window.onkeyup = function (evt) {
+    toggleKey(evt.keyCode, 0);
+};
+
+
+var controller = {
+    player1_magic : [
+        88,
+        89,
+        17,
+        13,
+        38,
+        40,
+        37,
+        39
+    ],
+    player2_magic : [
+        103,
+        105,
+        99,
+        97,
+        104,
+        98,
+        100,
+        102
+    ],
+    player1_bind : [
+        190,
+        191,
+        32,
+        13,
+        87,
+        83,
+        65,
+        68
+    ],
+    player2_bind : [
+        90,
+        88,
+        49,
+        50,
+        38,
+        40,
+        37,
+        39
+    ],
+    player1_state : [0,0,0,0,0,0,0,0],
+    player2_state : [0,0,0,0,0,0,0,0]
+};
+
+
+function toggleKey(key, state) {
+    var keys = {
+        KEY_A: 0,
+        KEY_B: 1,
+        KEY_SELECT: 2,
+        KEY_START: 3,
+        KEY_UP: 4,
+        KEY_DOWN: 5,
+        KEY_LEFT: 6,
+        KEY_RIGHT: 7
+    },
+    oldState, currentKey, player;
+
+    switch (key) {
+        case controller.player1_bind[keys.KEY_A] : 
+                oldState = controller.player1_state[keys.KEY_A];
+                currentKey = keys.KEY_A;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_B] : 
+                oldState = controller.player1_state[keys.KEY_B];
+                currentKey = keys.KEY_B;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_SELECT] : 
+                oldState = controller.player1_state[keys.KEY_SELECT];
+                currentKey = keys.KEY_SELECT;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_START] : 
+                oldState = controller.player1_state[keys.KEY_START];
+                currentKey = keys.KEY_START;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_UP] : 
+                oldState = controller.player1_state[keys.KEY_UP];
+                currentKey = keys.KEY_UP;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_DOWN] : 
+                oldState = controller.player1_state[keys.KEY_DOWN];
+                currentKey = keys.KEY_DOWN;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_LEFT] : 
+                oldState = controller.player1_state[keys.KEY_LEFT];
+                currentKey = keys.KEY_LEFT;
+                player = 1;
+                break;
+        case controller.player1_bind[keys.KEY_RIGHT] : 
+                oldState = controller.player1_state[keys.KEY_RIGHT];
+                currentKey = keys.KEY_RIGHT;
+                player = 1;
+                break;
+
+        
+
+        case controller.player2_bind[keys.KEY_A] : 
+                oldState = controller.player2_state[keys.KEY_A];
+                currentKey = keys.KEY_A;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_B] : 
+                oldState = controller.player2_state[keys.KEY_B];
+                currentKey = keys.KEY_B;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_SELECT] : 
+                oldState = controller.player2_state[keys.KEY_SELECT];
+                currentKey = keys.KEY_SELECT;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_START] : 
+                oldState = controller.player2_state[keys.KEY_START];
+                currentKey = keys.KEY_START;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_UP] : 
+                oldState = controller.player2_state[keys.KEY_UP];
+                currentKey = keys.KEY_UP;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_DOWN] : 
+                oldState = controller.player2_state[keys.KEY_DOWN];
+                currentKey = keys.KEY_DOWN;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_LEFT] : 
+                oldState = controller.player2_state[keys.KEY_LEFT];
+                currentKey = keys.KEY_LEFT;
+                player = 2;
+                break;
+        case controller.player2_bind[keys.KEY_RIGHT] : 
+                oldState = controller.player2_state[keys.KEY_RIGHT];
+                currentKey = keys.KEY_RIGHT;
+                player = 2;
+                break;
+    }
+    if (oldState !== state && typeof currentKey === 'number') {
+        console.log('change');
+        if (player === 1) {
+            socket.emit('control', [controller.player1_magic[currentKey], state]);
+            controller.player1_state[currentKey] = state;
+        } else {
+            socket.emit('control', [controller.player2_magic[currentKey], state]);
+            controller.player2_state[currentKey] = state;
+        }
+    }
+}
